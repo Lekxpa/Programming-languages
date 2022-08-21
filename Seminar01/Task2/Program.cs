a03e91a45b443a608a29d85be71a77a46c0b9c55
Lekxpa <LekxpaP@gmail.com>
2022-08-22 00:14:44 +0300

    add task2

---
+++ Seminar01/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+int result = 0;
+if (a > b)
+{
+    result = a;
+    Console.WriteLine($"max = {result}");
+}
+else
+{
+    result = b;
+    Console.WriteLine($"max = {result}");
+}

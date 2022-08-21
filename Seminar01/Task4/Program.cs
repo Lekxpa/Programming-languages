a6210ecde3109d45eac3843f9139988ed199de21
Lekxpa <LekxpaP@gmail.com>
2022-08-22 00:51:49 +0300

    add Task4

---
+++ Seminar01/Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число c: ");
+int c = int.Parse(Console.ReadLine());
+int result = 0;
+if (a > b)
+{
+    if (a > c)
+    {
+      result = a;
+    }
+    else
+    {
+        result = c;
+    }
+}
+else
+{
+    if (b > c)
+    {
+         result = b;
+    }
+    else
+    {
+        result = c;
+    }
+}
+ Console.WriteLine($"max = {result}");

74c0bc9dd0350a4a21d4607e40fbb10d95d21614
Lekxpa <LekxpaP@gmail.com>
2022-08-22 11:43:42 +0300

    add file Task8

---
+++ Seminar01/Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int current = 1;
+while (current <= num)
+{
+    if (current % 2 == 0)
+    {
+        Console.Write($"{current} ");
+        current++;  
+    }
+    else
+    {
+         current++;
+    }
+}

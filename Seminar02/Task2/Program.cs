246f301a143b752b454103333f5043f064820505
Lekxpa <LekxpaP@gmail.com>
2022-08-23 15:12:49 +0300

    add Task2 Seminar02

---
+++ Seminar02/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру 
+//(слева направо) заданного числа или сообщает, что третьей цифры нет.
+//645 -> 6
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int current;
+
+if (num < 0) num = -num;
+
+if (num >=100)
+{
+    current = (num % 1000) / 100;
+    Console.WriteLine(current);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}

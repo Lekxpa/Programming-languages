f45b00f5cfa07215a97199b319c964d7532faaa9
Lekxpa <LekxpaP@gmail.com>
2022-08-26 15:40:05 +0300

    add file Task03 in Seminar03

---
+++ Seminar03/Task03/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23  Напишите программу, которая принимает на вход 
+//число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int current = 1;
+
+while (current <= num)
+{
+    Console.Write($"{current*current*current}, ");
+    current++;
+}
+Console.WriteLine();
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i*i*i}, ");
+}
+Console.WriteLine();

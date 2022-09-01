14fa26c63ef9352342da27c9e8c7fccab88983f7
Lekxpa <LekxpaP@gmail.com>
2022-09-02 00:52:08 +0300

    add file Task02 in Seminar04

---
+++ Seminar04/Task02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход 
+//число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int GrNumber(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"{GrNumber(number)}");

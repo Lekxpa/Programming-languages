6685d7a9adaf3bc8c4193f3cf94552467504f877
Lekxpa <LekxpaP@gmail.com>
2022-09-04 16:05:36 +0300

    add file Taskstar in Seminar04

--- Seminar04/Taskstar/Program.cs
+++ Seminar04/Taskstar/Program.cs
@@ -1,3 +1,60 @@
 ﻿// Напишите функцию, которая принимает одно число - высоту елочки и 
 //рисует ее в консоли звездочками
 
+Console.Write("Укажите высоту елочки: ");
+int heigth = int.Parse(Console.ReadLine());
+
+int sp = heigth - 1;  // пробелы
+    int h = 1;          // количество звездочек
+    for (int i = 0; i < heigth; i++)
+    {
+        for(int m=0; m <= sp; m++)
+        {
+            Console.Write(" ");
+  
+        }
+        for(int n = 0; n < h; n++)
+        {
+            Console.Write("*");
+        
+        }  
+    Console.WriteLine();
+    sp = sp - 1;
+    h = h + 2;
+    }
+  
+  
+
+
+
+
+
+
+//int StarH(int haut)
+//{
+    // int sp = haut - 1;  // пробелы
+    //int h = 1;          // количество звездочек
+    //for (int i = 0; i <= haut; i++)
+   // {
+       // for(int m=0; m <= sp; m++)
+       // {
+       //     Console.Write(" ");
+      //      sp = sp - 1;
+      //  }
+        //return sp;
+      //  for(int n = 0; n <= h; n++)
+      //  {
+       //     Console.Write("*");
+      //      h = h + 2;
+      //  }  
+        //return h;
+  //  Console.WriteLine();
+  //  }
+  
+  
+
+//}
+
+//Console.Write("Укажите высоту елочки: ");
+//int heigth = int.Parse(Console.ReadLine());
+//Console.Write(StarH);

821bc9faec6cbe1d6fb825f6a3a1d3313af76edb
Lekxpa <LekxpaP@gmail.com>
2022-09-05 23:47:24 +0300

    add file Task02 in Seminar05

---
+++ Seminar05/Task01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int[] MyArray(int size, int min, int max)
+{
+int[] res = new int[size];
+//int count = 0;
+for (int i =0; i < size; i++)
+{
+    res[i] = new Random().Next(min, max +1);
+  
+    //if (res[i] % 2 == 0) count++;
+}
+//Console.WriteLine($"{count}");
+return res;
+}
+
+int[] EvenNumbers(int[] array)
+
+//void EvenNumbers(int[] array)
+{
+   int count = 0;
+    //foreach(int m in array)
+    for (int i = 0; i < array.Length; i++)
+    {
+        //count += m % 2  == 0 ? 1 : 0;
+        if (array[i] % 2 == 0)  count++;
+    }
+    Console.WriteLine($"{count}");
+    return array;
+}
+
+Console.Write("Введите количество элементов в массиве: ");
+int number = int.Parse(Console.ReadLine());
+
+int[] EvenArray = MyArray(number, 100, 1000);
+int[] result = EvenNumbers(EvenArray);
+Console.WriteLine(String.Join(" ", EvenArray));
+//Console.WriteLine(String.Join(" ", result));
+//EvenNumbers(EvenArray);
+//Console.WriteLine($"{count}");
---
+++ Seminar05/Task02/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными 
+//числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+int[] MonArray(int size, int min, int max)
+{
+    int[] res = new int[size];
+    for (int i =0; i < size; i++)
+        {
+            res[i] = new Random().Next(min, max +1);
+        }
+    return res;
+}
+
+void ENNumbers(int[] array)
+{
+    int res = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) res = res + array[i];
+    }
+  Console.WriteLine($"{res}");
+}
+
+Console.Write("Введите количество элементов в массиве: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите значение начала интервала: ");
+int beginline = int.Parse(Console.ReadLine());
+Console.Write("Введите значение окончания интервала: ");
+int endline = int.Parse(Console.ReadLine());
+
+int[] EvenArray = MonArray(number, beginline, endline);
+Console.WriteLine(String.Join(" ", EvenArray));
+ENNumbers(EvenArray);

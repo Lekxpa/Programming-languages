7d627c158be4fcbf3bb2a1fd51ce0aa6cda021e6
Lekxpa <LekxpaP@gmail.com>
2022-09-08 09:51:06 +0300

    correct file Task03 in Seminar05

--- Seminar05/Task03/Program.cs
+++ Seminar05/Task03/Program.cs
@@ -2,54 +2,49 @@
 //между максимальным и минимальным элементом массива.
 // [3 7 22 2 78] -> 76
 
+//не получилось в числах сделать после запятой поменьше знаков
 
-int[] MonArray(int size, int min, int max)
+double[] MonArray (int size, double min, double max)
 {
-    int[] res = new int[size];
-    for (int i =0; i < size; i++)
-        {
-            res[i] = new Random().Next(min, max +1);
-        }
-    return res;
+    double[] res = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble()*10;  
+    }
+return res;
 }
-
-void MinNumbers(int[] array)
+void MinMaxNumbers(Double[] array)
 {
-    int mn =  array[0];
+    Double mn =  array[0];
+    Double mx =  array[0];
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i] < mn) mn = array[i];
     }
-  Console.WriteLine($"Минимальное значение: {mn}");
-}
+    Console.WriteLine();
+    Console.WriteLine($"Минимальное значение: {mn}");
 
-void MaxNumbers(int[] array)
-{
-    int mx =  array[0];
-    for (int i = 0; i < array.Length; i++)
+    for (int j = 0; j < array.Length; j++)
     {
-        if (array[i] > mx) mx = array[i];
+        if (array[j] > mx) mx = array[j];
     }
-    //return mx;
-  Console.WriteLine($"Максимальное значение: {mx}");
-}
+    Console.WriteLine();
+    Console.WriteLine($"Максимальное значение: {mx}");
 
-//int result = MaxNumbers - MinNumbers;
-//Console.WriteLine(result);
+    Double result = mx - mn;
+    
+    Console.WriteLine();
+    Console.WriteLine($"Разница между max и min эл-тами массива: {result}");
+    Console.WriteLine();
+}
 
 Console.Write("Введите количество элементов в массиве: ");
 int number = int.Parse(Console.ReadLine());
 Console.Write("Введите значение начала интервала: ");
-int beginline = int.Parse(Console.ReadLine());
+Double beginline = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите значение окончания интервала: ");
-int endline = int.Parse(Console.ReadLine());
+Double endline = Convert.ToDouble(Console.ReadLine());
 
-int[] EvenArray = MonArray(number, beginline, endline);
+Double[] EvenArray = MonArray(number, beginline, endline);
 Console.WriteLine(String.Join(" ", EvenArray));
-MinNumbers(EvenArray);
-MaxNumbers(EvenArray);
-
-//int res1 = MinNumbers(EvenArray);
-
-//int result = mx - mn;
-//Console.WriteLine(res1);
+MinMaxNumbers(EvenArray);

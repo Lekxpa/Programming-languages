4f175b9b00c95c3b18a280523d70a86cc3b0b8a6
Lekxpa <LekxpaP@gmail.com>
2022-09-11 00:52:33 +0300

    correct file task01 in seminar06

--- Seminar06/Task01/Program.cs
+++ Seminar06/Task01/Program.cs
@@ -4,18 +4,25 @@
 //1, -7, 567, 89, 223-> 3
 
 
+int PositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;  
+    }
+    return result;
+}
 
 Console.Write("Укажите количество чисел: ");
-int count = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
 
+int[] FillArray = new int[number];
+for(int i = 0; i < number; i++)
+    {
+        Console.Write("Введите {0}-й элемент: " ,  i + 1);
+        FillArray[i] = int.Parse(Console.ReadLine());
+    }
 
-int[] array = new int[count];
-for(int i = 0; i < count; i++)
-{
-    Console.Write("Введите {0}-й элемент: " ,  i + 1);
-    array[i] = int.Parse(Console.ReadLine());
-}
-int result = 0;
-for (int i = 0; i < array.Length; i++) 
-if (array[i] > 0) result++;  
-Console.WriteLine($"Количество положительных чисел = {result}");
+int count = PositiveNumbers(FillArray);
+Console.WriteLine($"Количество положительных чисел = {count}");
--- Seminar06/Task02/Program.cs
+++ Seminar06/Task02/Program.cs
@@ -4,3 +4,66 @@
 //задаются пользователем.
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+int[,] MyArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+    }
+}
+
+
+void PointOfIntersection (int[,] array)
+{
+//FirstLine SecondLine
+    
+    int x = (n2 - n1) / (m1 - m2);
+
+    int y = m1 * (n2 - n1) / (m1 - m2) + n1;
+    Console.Write($"{x}, {y}");
+}
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("Введите минимальное значение: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write("Введите максимальное значение: ");
+int max = int.Parse(Console.ReadLine());
+
+//Console.Write("Введите первое значение - k1: ");
+//int k1 = int.Parse(Console.ReadLine());
+//Console.Write("Введите второе значение - b1: ");
+//int b1 = int.Parse(Console.ReadLine());
+//Console.Write("Введите третье значение - k2: ");
+//int k2 = int.Parse(Console.ReadLine());
+//Console.Write("Введите четвертое значение - b2: ");
+//int b2 = int.Parse(Console.ReadLine());
+
+//int[,] EvenArray = PointOfIntersection[,];
+//Console.WriteLine(String.Join(" ", EvenArray));
+//CrossLine(EvenArray);
+
+int[,] MonArray = MyArray(m, n, min, max);
+PrintArray(MonArray);
+PointOfIntersection(MonArray);

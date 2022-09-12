591aa285515c5105406f34a4e81f2a9a6f2f13e1
Lekxpa <LekxpaP@gmail.com>
2022-09-12 14:05:30 +0300

    correct file taskstar in seminar06

--- Seminar06/TaskStar/Program.cs
+++ Seminar06/TaskStar/Program.cs
@@ -3,64 +3,83 @@
 // элемента против часовой стрелки
 
 
-//int[,] MyArray(int m, int n, int min, int max)
-int[,] MyArray()
+int[,] MyArray(int m, int n, int min, int max)
 {
-    int[,] result = new int[,];
+    int[,] result = new int[m,n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i,j] = new Random().Next(0, 0);
+            result[i,j] = new Random().Next(0, 2);
         }
     }
     return result;
 }
 
-void PrintImage(int[,] image)
+void PrintArray(int[,] array)
 {
-    for (int i = 0; i < image.GetLength(0); i ++)
-{
-    for (int j = 0; j < image.GetLength(1); j ++)
+    for (int rows = 0; rows < array.GetLength(0); rows++)
     {
-        if (image[i, j] == 0) Console.Write($" ");
-        else Console.Write($"+");
-    }
-    
-    Console.WriteLine();
-}
-
-}
-
-// закрашиваем область внутри знаков
-
-void FillImage (int row, int col)
-{
-   // int row = MArray.GetLength(0);
-    //int col = 0;
-    if(MyArray[row, col] == 0)
-    {
-        MyArray[row, col] = 1;
-        FillImage(row-1, col); // шаг влево
-        FillImage(row, col-1); // шак вниз
-        FillImage(row+1, col); // шаг вправо
-        FillImage(row, col+1); // шак вверх
+        for (int cols = 0; cols < array.GetLength(1); cols++)
+        {
+            Console.Write($"{array[rows,cols]} ");
+        }
+        Console.WriteLine();
     }
 }
+
+int FillImage (int[,] Fillarray)
+{
+    int step = Fillarray[Fillarray.GetLength(0),0];
+    for (int row = Fillarray.GetLength(0); row < Fillarray.GetLength(0); row--)
+    {
+        for (int col = 0; col < Fillarray.GetLength(1); col++)
+        {
+            if (step == 0)
+            {
+                step = 1;
+                step = Fillarray[row - 1, col];
+            }
+            if (step == 0)
+            {
+                step = 1;
+                step = Fillarray[row, col-1];
+            }
+                if (step == 0)
+            {
+                step = 1;
+                step = Fillarray[row + 1, col];
+            }
+            if (step == 0)
+            {
+                step = 1;
+                step = Fillarray[row, col + 1];
+            }
+            Console.Write($"{Fillarray[row,col]} ");
+        }
+    }
+    return step;
+}
+
+//void FillImage (int row, int col)
+//{
+   // if(MyArray[row, col] = 0)
+    //{
+     //   MyArray[row, col] = 1;
+      //  FillImage(row-1, col); // шаг влево
+      //  FillImage(row, col-1); // шаг вниз
+       // FillImage(row+1, col); // шаг вправо
+       // FillImage(row, col+1); // шаг вверх
+   // }
+//}
 
 Console.Write("Введите количество строк: ");
 int numrows = int.Parse(Console.ReadLine());
-
 Console.Write("Введите количество столбцов: ");
 int numcols = int.Parse(Console.ReadLine());
 
-//Console.Write("Введите минимальное значение: ");
-//int minEl = int.Parse(Console.ReadLine());
-
-//Console.Write("Введите максимальное значение: ");
-//int maxEl = int.Parse(Console.ReadLine());
-
-int[,] MonArray = MyArray(numrows, numcols, 0, 1);
-PrintImage(MonArray);
-FillImage (MonArray.GetLength(0), 0);
-PrintImage(MonArray);
+int[,] MonArray = MyArray(numrows, numcols, 0, 2);
+PrintArray(MonArray);
+FillImage(MonArray);
+Console.WriteLine();
+PrintArray(MonArray);

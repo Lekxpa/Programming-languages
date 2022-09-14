e76191e0a6ab1cde9b3e4889498b9b855cef5efa
Lekxpa <LekxpaP@gmail.com>
2022-09-14 16:07:07 +0300

    add file03 in seminar07

---
+++ Seminar07/Task02/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 50. Напишите программу, которая на вход 
+//принимает число и генерирует случайный двумерный массив, 
+//и возвращает индексы этого элемента или же указание, что такого 
+//элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+int[,] MyArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int cols = 0; cols < array.GetLength(1); cols++)
+        {
+            Console.Write($"{array[rows,cols]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SearchNumber(int[,] Array)
+{
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    {
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            for (int j = 0; j < Array.GetLength(0); j++)
+            {
+                if(num = Array[i,j])
+                {
+                    Console.WriteLine($"Индексы заданного числа: ({i}, {j}) ");
+                    //Console.WriteLine("Такого числа в массиве нет");
+                }
+                else
+                {
+                    Console.WriteLine("Такого числа в массиве нет");
+                    //Console.WriteLine($"Индексы заданного числа: ({i}, {j}) ");
+                }
+             }
+             //if (num != Array[i,j]) Console.WriteLine("Такого числа в массиве нет");
+        }
+         //Console.WriteLine("Такого числа в массиве нет");
+        //else Console.WriteLine("Такого числа в массиве нет");
+    }
+    return num;
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int lines = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] MonArray = MyArray(lines, columns, -100, 100);
+PrintArray(MonArray);
+SearchNumber(MonArray);
---
+++ Seminar07/Task03/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+double[,] MyArray(int m, int n, int min, int max)
+{
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(min, max + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int rows = 0; rows < array.GetLength(0); rows++)
+    {
+        for (int cols = 0; cols < array.GetLength(1); cols++)
+        {
+            Console.Write($"{array[rows,cols]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ArithmeticAverage (double[,] Array)
+{
+    for (int j = 0; j < Array.GetLength(1); j++)
+    {
+        double result = 0;
+        for (int i = 0; i < Array.GetLength(0); i++)
+        {
+            result = result + Array[i, j];
+        }
+        result = result / Array.GetLength(1);
+        Console.Write($"{result:F2}  ");
+    }
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int lines = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+Console.Write("Введите значение начала интервала: ");
+int beginline = int.Parse(Console.ReadLine());
+Console.Write("Введите значение окончания интервала: ");
+int endline = int.Parse(Console.ReadLine());
+
+
+double[,] MonArray = MyArray(lines, columns, beginline, endline);
+PrintArray(MonArray);
+ArithmeticAverage(MonArray);

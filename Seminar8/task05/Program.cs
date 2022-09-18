a3171fcca84d8c4eb558a8d256c3106711fe469d
Lekxpa <LekxpaP@gmail.com>
2022-09-18 23:35:16 +0300

    add file Task05 in Seminar8

--- Seminar8/task05/Program.cs
+++ Seminar8/task05/Program.cs
@@ -6,53 +6,24 @@
 //11 16 15 06
 //10 09 08 07
 
-//Console.Clear();
-//Console.WriteLine($"Задача 62: Заполните спирально массив 4 на 4.");
-
-//void SpiralArray (int[,] array)
-int[,] SpiralArray(int[,] array)
+int[,] SpiralArray(int m, int n, int min, int max)
 {
-    //Console.Write("Введите количество строк и столбцов в массиве: ");
-    //int numLineAndCols = int.Parse(Console.ReadLine());
-    //int[,] MyArray = new int[int m, int n, int min, int max];
-
-
+    int[,] result = new int[m,n];
     int temp = 1;
     int i = 0;
     int j = 0;
 
-    while (temp <= array.GetLength(0) * array.GetLength(1))
+    while (temp <= result.GetLength(0) * result.GetLength(1))
     {
-        array[i, j] = temp;
+        result[i, j] = temp;
         temp++;
-        if (i <= j + 1 && i + j < array.GetLength(1) - 1)
-        j++;
-        else if (i < j && i + j >= array.GetLength(0) - 1)
-        i++;
-        else if (i >= j && i + j > array.GetLength(1) - 1)
-        j--;
-        else
-        i--;
+        if (i <= j + 1 && i + j < result.GetLength(1) - 1) j++;
+        else if (i < j && i + j >= result.GetLength(0) - 1) i++;
+        else if (i >= j && i + j > result.GetLength(1) - 1) j--;
+        else i--;
     }
-    return array; //MyArray;
+    return result;
 }
-
-//WriteArray(sqareMatrix);
-
-//void WriteArray (int[,] array)
-//{
-//  for (int i = 0; i < array.GetLength(0); i++)
-//  {
-//    for (int j = 0; j < array.GetLength(1); j++)
- //   {
- //     if (array[i,j]  < 10)
- //     Console.Write($" {array[i,j]}  ");
-
-  //    else Console.Write($"{array[i,j]}  ");
-  //  }
-  //  Console.WriteLine();
- // }
-//}
 
 void PrintArray(int[,] array)
 {
@@ -60,21 +31,13 @@ void PrintArray(int[,] array)
     {
         for (int cols = 0; cols < array.GetLength(1); cols++)
         {
-            if (array[rows,cols]  < 10)
-            Console.Write($" {array[rows,cols]}  ");
+            if (array[rows,cols] < 10)
+            Console.Write($"0{array[rows,cols]}  ");
             else Console.Write($"{array[rows,cols]}  ");
         }
+        Console.WriteLine();
     }
 }
 
-
-  Console.Write("Введите количество строк и столбцов в массиве: ");
-    int numLineAndCols = int.Parse(Console.ReadLine());
-    int numbers = numLineAndCols * numLineAndCols;
-    int[,] MyArray = SpiralArray(numLineAndCols, numLineAndCols, 1, numbers);
-//Console.Write("Введите количество строк и столбцов в массиве: ");
-//int numLineAndCols = int.Parse(Console.ReadLine());
-
 int[,] MonArray = SpiralArray(4, 4, 1, 16);
-PrintArray(MyArray);
-//OrderOfElementsOfArray(MonArray);
+PrintArray(MonArray);

be471ad6f0219ac3828cd7f9209ab136c5c81e8f
Lekxpa <LekxpaP@gmail.com>
2022-09-22 00:36:27 +0300

    correct file Task02 in Seminar09

--- Seminar09/Task02/Program.cs
+++ Seminar09/Task02/Program.cs
@@ -2,3 +2,19 @@
 //с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 9
 
+int FunctionOfAkkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionOfAkkerman(m - 1, 1);
+    else return FunctionOfAkkerman(m - 1, FunctionOfAkkerman(m, n - 1));
+}
+
+Console.Write("Введите число m: ");
+int NumM = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int NumN = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+if (NumM < 0 || NumN < 0)  Console.WriteLine($"Можно использовать только положительные числа");
+else Console.Write($"Функция Аккермана: {FunctionOfAkkerman(NumM, NumN)}");
+Console.WriteLine();
